a7ab84b9eaebe79e4098e90d9d57a9068834c415
Sunny Karira <sunnykarira@outlook.com>
2016-06-16 16:11:23 +0530

    Add POST /books route

--- controllers/manage.js
+++ controllers/manage.js
@@ -34,4 +34,59 @@ module.exports = function (router) {
     	
     });
 
+    router.get('/books/add', function(req, res){
+    	Category.find({}, function(err, categories){
+    		if(err) console.log(err);
+    		var model = {
+    			categories: categories
+    		}
+    		res.render('manage/books/add', model);
+    	});
+    });
+
+    router.post('/books', function(req, res){
+    	// Remove excess space
+    	var title =  req.body.title && req.body.title.trim();
+        var category = req.body.category && req.body.category.trim();
+        var author = req.body.author && req.body.author.trim();
+        var publisher = req.body.publisher && req.body.publisher.trim();
+        var price = req.body.price && req.body.price.trim();
+        var description = req.body.description && req.body.description.trim();
+        var cover = req.body.cover && req.body.cover.trim();
+
+        if (title == '' || price == ''){
+            req.flash('error', 'Please fill out required fields');
+            res.location('/manage/books/add');
+            res.redirect('/manage/books/add');
+        }
+
+        if (isNaN(price)){
+            req.flash('error', 'Price must be a number');
+            res.location('/manage/books/add');
+            res.redirect('/manage/books/add');
+        }
+
+        var newBook = new Book({
+            title: title,
+            category: category,
+            author: author,
+            publisher: publisher,
+            price: price,
+            description: description,
+            cover: cover
+        });
+
+        newBook.save(function(err){
+            if(err){
+                console.log('save error', err);
+            }
+
+            req.flash('success', 'Book Added');
+            res.location('/manage/books');
+            res.redirect('/manage/books');
+        });
+
+
+    });
+
 };
